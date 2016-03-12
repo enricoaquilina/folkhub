522e906767652b7de631ae61cb01502c08673343
profmouse <tappiera00@gmail.com>
2016-03-12 15:50:19 +0100

    added angular, mongoose, heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus');
     logger = require('morgan');
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
@@ -30,18 +31,42 @@ app.use(stylus.middleware(
 //go ahead and serve the file.
 app.use(express.static(__dirname + '/public'));
 
+//connect to mongoose
+if(env === 'development'){
+  mongoose.connect('mongodb://localhost/folkhub');
+}else{
+  mongoose.connect('mongodb://profmouse:folkhub@ds011449.mlab.com:11449/folkhub');
+}
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function cb(){
+  console.log('folkhub db opened..');
+});
+var messageSchema = mongoose.Schema({message:String});
+var messageModel = mongoose.model('Message', messageSchema);
+
+var mongoMessage;
+messageModel.findOne().exec(function(err, result){
+  mongoMessage = result.message;
+});
+
 /*add a route which delivers the index page for all routes
 asterisk will match all routes which are not previously defined
 (kinda like the default in a switch statement)
 
 server side just serves index..on the client it handles the routing
 */
+app.get('/partials/:path', function(req, res){
+  res.render('partials/'+req.params.path);
+});
 app.get('*', function(req, res){
-  res.render('index');
+  res.render('index', {message:mongoMessage});
 })
 
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function(){
   console.log("App started on port "+port);
 });
