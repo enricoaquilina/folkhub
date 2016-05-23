086f6b730559fde4530c2286eb10e63100da1b9c
profmouse <tappiera00@gmail.com>
2016-05-24 00:51:26 +0200

    test heroku sessions pt.2

--- server/config/express.js
+++ server/config/express.js
@@ -20,9 +20,15 @@ module.exports = function(app, config, req, res, next){
 
   if (process.env.REDISTOGO_URL) {
     var rtg = require("url").parse(process.env.REDISTOGO_URL);
+
+    host = "redis://redistogo:df3994bfcc3f703ee6a216c5ffa28cf0@"+rtg.host;
+    host = rtg.port;
+
     console.log(rtg.host);
     console.log(rtg.port);
-    redisSession = redis.createClient(rtg.host, rtg.port);
+
+    redisSession = redis.createClient(host, port);
+
     console.log(rtg.auth.split(":")[1]);
     // redis.auth('df3994bfcc3f703ee6a216c5ffa28cf0');
   } else {
