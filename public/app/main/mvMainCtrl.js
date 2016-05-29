8b6e1b59e2945354e5cb848a387272b9d5bea634
profmouse <tappiera00@gmail.com>
2016-05-29 03:41:50 +0200

    testing ws connection on heroku

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -4,7 +4,7 @@ angular.module('app').controller('mvMainCtrl', function($scope){
   var host = location.origin
               .replace(/^http/, 'ws')
               .replace('5000','5001');
-
+  console.log(host);
   var ws = new WebSocket(host);
   ws.onopen = function(event) {
     console.log("Connection established");
